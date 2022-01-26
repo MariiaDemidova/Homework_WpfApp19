59b2f7d6e93e41d8887c31be77eadb0cc3d70c7d
Mariia <mariiademidova@outlook.com>
2022-01-26 18:20:42 +0300

    Добавьте файлы проекта.

---
+++ Homework_WpfApp19/Models/CircleCalc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_WpfApp19.Models
+{
+    static class CircleCalc
+    {
+        public static double Calc(double r) => 2 * Math.PI * r;
+    }
+}
---
+++ Homework_WpfApp19/ViewModels/MainWindowViewModel.xaml.cs
@@ -0,0 +1,70 @@
+﻿using Homework_WpfApp19.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Homework_WpfApp19.ViewModels
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindowViewModel.xaml
+    /// </summary>
+    public partial class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName] string PropertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(PropertyName));
+        }
+
+        private double radius;
+        public double Radius
+        {
+            get => radius;
+            set
+            {
+                radius = value;
+                OnPropertyChanged();
+            }
+        }
+        private double length;
+        public double Length
+        {
+            get => length;
+            set
+            {
+                length = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand AddCommand { get; }
+        private void OnAddCommandExecute(object p)
+        {
+            Length = CircleCalc.Calc(Radius);
+        }
+        private bool CanAddCommandExecuted(object p)
+        {
+            if (radius > 0)
+                return true;
+            else
+                return false;
+        }
+        public MainWindowViewModel()
+        {
+            AddCommand = new RelayCommand(OnAddCommandExecute, CanAddCommandExecuted);
+        }
+    }
+}
